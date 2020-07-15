6a861643ed2d8e3871369b540be47309d8947f66
Tae Kimura <sheepman7893@gmail.com>
2020-07-14 20:48:41 -0700

    added-chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -2,60 +2,74 @@ import React, { Component } from "react";
 // import Wheel from "../assets/Wheel.png";
 
 class Canvas extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        this.state = {
+            averageAnswers: [],
+        };
     }
 
     componentDidMount() {
         const canvas = this.refs.canvas;
         const ctx = canvas.getContext("2d");
-        var X = canvas.width / 2;
-        var Y = canvas.height / 2;
+        const X = canvas.width / 2;
+        const Y = canvas.height / 2;
+        const zero = 185;
+        const one = 170;
+        const two = 155;
+        const three = 140;
+        const four = 125;
+        const five = 110;
+        const six = 95;
+        const seven = 80;
+        const eight = 65;
+        const nine = 50;
+        const ten = 35;
         ctx.strokeStyle = '#FFD700';
         ctx.fillStyle = '#FFD700';
+        //circle0
+        ctx.beginPath();
+        ctx.arc(X, Y, zero, 0, 2 * Math.PI, false);
+        ctx.stroke();
         //circle1
         ctx.beginPath();
-        ctx.arc(X, Y, 185, 0, 2 * Math.PI, false);
+        ctx.arc(X, Y, one, 0, 2 * Math.PI, false);
         ctx.stroke();
         //circle2
         ctx.beginPath();
-        ctx.arc(X, Y, 170, 0, 2 * Math.PI, false);
+        ctx.arc(X, Y, two, 0, 2 * Math.PI, false);
         ctx.stroke();
         //circle3
         ctx.beginPath();
-        ctx.arc(X, Y, 155, 0, 2 * Math.PI, false);
+        ctx.arc(X, Y, three, 0, 2 * Math.PI, false);
         ctx.stroke();
         //circle4
         ctx.beginPath();
-        ctx.arc(X, Y, 140, 0, 2 * Math.PI, false);
+        ctx.arc(X, Y, four, 0, 2 * Math.PI, false);
         ctx.stroke();
         //circle5
         ctx.beginPath();
-        ctx.arc(X, Y, 125, 0, 2 * Math.PI, false);
+        ctx.arc(X, Y, five, 0, 2 * Math.PI, false);
         ctx.stroke();
         //circle6
         ctx.beginPath();
-        ctx.arc(X, Y, 110, 0, 2 * Math.PI, false);
+        ctx.arc(X, Y, six, 0, 2 * Math.PI, false);
         ctx.stroke();
         //circle7
         ctx.beginPath();
-        ctx.arc(X, Y, 95, 0, 2 * Math.PI, false);
+        ctx.arc(X, Y, seven, 0, 2 * Math.PI, false);
         ctx.stroke();
         //circle8
         ctx.beginPath();
-        ctx.arc(X, Y, 80, 0, 2 * Math.PI, false);
+        ctx.arc(X, Y, eight, 0, 2 * Math.PI, false);
         ctx.stroke();
         //circle9
         ctx.beginPath();
-        ctx.arc(X, Y, 65, 0, 2 * Math.PI, false);
+        ctx.arc(X, Y, nine, 0, 2 * Math.PI, false);
         ctx.stroke();
         //circle10
         ctx.beginPath();
-        ctx.arc(X, Y, 50, 0, 2 * Math.PI, false);
-        ctx.stroke();
-        //circle10
-        ctx.beginPath();
-        ctx.arc(X, Y, 35, 0, 2 * Math.PI, false);
+        ctx.arc(X, Y, ten, 0, 2 * Math.PI, false);
         ctx.fill();
         //bars with loop
         // ctx.translate(X, Y);
@@ -70,14 +84,18 @@ class Canvas extends Component {
         // ctx.stroke();
         // ctx.restore();
 
+        // void ctx.rotate(angle);
+        // angle parameters...
+        // degree * Math.PI / 180
+
         //accueil
         ctx.translate(X, Y);
         ctx.strokeStyle = "#006c8b";
         ctx.lineWidth = 8;
         ctx.beginPath();
         ctx.rotate(270 * Math.PI / 180);
-        ctx.moveTo(185, 0);
-        ctx.lineTo(35, 0);
+        ctx.moveTo(zero, 0);
+        ctx.lineTo(ten, 0);
         ctx.stroke();
         ctx.restore();
 
@@ -85,8 +103,8 @@ class Canvas extends Component {
         ctx.strokeStyle = "#50959a";
         ctx.beginPath();
         ctx.rotate(Math.PI / 6);
-        ctx.moveTo(185, 0);
-        ctx.lineTo(35, 0);
+        ctx.moveTo(eight, 0);
+        ctx.lineTo(ten, 0);
         ctx.stroke();
         ctx.restore();
 
@@ -94,8 +112,8 @@ class Canvas extends Component {
         ctx.strokeStyle = "#005c41";
         ctx.beginPath();
         ctx.rotate(Math.PI / 6);
-        ctx.moveTo(185, 0);
-        ctx.lineTo(35, 0);
+        ctx.moveTo(zero, 0);
+        ctx.lineTo(ten, 0);
         ctx.stroke();
         ctx.restore();
 
@@ -103,8 +121,8 @@ class Canvas extends Component {
         ctx.strokeStyle = "#32774b";
         ctx.beginPath();
         ctx.rotate(Math.PI / 6);
-        ctx.moveTo(185, 0);
-        ctx.lineTo(35, 0);
+        ctx.moveTo(one, 0);
+        ctx.lineTo(ten, 0);
         ctx.stroke();
         ctx.restore();
 
@@ -112,8 +130,8 @@ class Canvas extends Component {
         ctx.strokeStyle = "#5b9e51";
         ctx.beginPath();
         ctx.rotate(Math.PI / 6);
-        ctx.moveTo(185, 0);
-        ctx.lineTo(35, 0);
+        ctx.moveTo(two, 0);
+        ctx.lineTo(ten, 0);
         ctx.stroke();
         ctx.restore();
 
@@ -121,8 +139,8 @@ class Canvas extends Component {
         ctx.strokeStyle = "#c54619";
         ctx.beginPath();
         ctx.rotate(Math.PI / 6);
-        ctx.moveTo(185, 0);
-        ctx.lineTo(35, 0);
+        ctx.moveTo(three, 0);
+        ctx.lineTo(ten, 0);
         ctx.stroke();
         ctx.restore();
 
@@ -130,8 +148,8 @@ class Canvas extends Component {
         ctx.strokeStyle = "#c45621";
         ctx.beginPath();
         ctx.rotate(Math.PI / 6);
-        ctx.moveTo(185, 0);
-        ctx.lineTo(35, 0);
+        ctx.moveTo(one, 0);
+        ctx.lineTo(ten, 0);
         ctx.stroke();
         ctx.restore();
 
@@ -139,8 +157,8 @@ class Canvas extends Component {
         ctx.strokeStyle = "#e07c27";
         ctx.beginPath();
         ctx.rotate(Math.PI / 6);
-        ctx.moveTo(185, 0);
-        ctx.lineTo(35, 0);
+        ctx.moveTo(one, 0);
+        ctx.lineTo(ten, 0);
         ctx.stroke();
         ctx.restore();
 
@@ -148,8 +166,8 @@ class Canvas extends Component {
         ctx.strokeStyle = "#8c191c";
         ctx.beginPath();
         ctx.rotate(Math.PI / 6);
-        ctx.moveTo(185, 0);
-        ctx.lineTo(35, 0);
+        ctx.moveTo(two, 0);
+        ctx.lineTo(ten, 0);
         ctx.stroke();
         ctx.restore();
 
@@ -157,8 +175,8 @@ class Canvas extends Component {
         ctx.strokeStyle = "#8c191c";
         ctx.beginPath();
         ctx.rotate(Math.PI / 6);
-        ctx.moveTo(185, 0);
-        ctx.lineTo(35, 0);
+        ctx.moveTo(three, 0);
+        ctx.lineTo(ten, 0);
         ctx.stroke();
         ctx.restore();
 
@@ -166,8 +184,8 @@ class Canvas extends Component {
         ctx.strokeStyle = "#b92351";
         ctx.beginPath();
         ctx.rotate(Math.PI / 6);
-        ctx.moveTo(185, 0);
-        ctx.lineTo(35, 0);
+        ctx.moveTo(one, 0);
+        ctx.lineTo(ten, 0);
         ctx.stroke();
         ctx.restore();
 
@@ -175,14 +193,10 @@ class Canvas extends Component {
         ctx.strokeStyle = "#00506d";
         ctx.beginPath();
         ctx.rotate(Math.PI / 6);
-        ctx.moveTo(185, 0);
-        ctx.lineTo(35, 0);
+        ctx.moveTo(two, 0);
+        ctx.lineTo(ten, 0);
         ctx.stroke();
         ctx.restore();
-
-
-
-
 
 
         // var gradient = ctx.createRadialGradient(75, 50, 5, 90, 70, 150);

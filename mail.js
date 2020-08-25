36df7e6585535ab995fb6310379321c9b55064e0
Tae Kimura <sheepman7893@gmail.com>
2020-08-25 15:18:38 -0700

    modified-icon-yesno-answer-delete-apply-button

--- mail.js
+++ mail.js
@@ -11,10 +11,34 @@ const getEmailData = (to, name, template, image) => {
                 to,
                 cc: "Tae Kimura <sheepman7893@gmail.com>",
                 // bcc: "Tae Kimura <sheepman7893@gmail.com>",
-                subject: `Universal prosperity blueprint`,
+                subject: `Your Universal Prosperity BluePrint`,
                 html: `
-                Hello ${name}. <br/>
-                This is your universal prosperity blueprint.
+                Congratulations! Here's your Universal Prosperity Blueprint.
+                <br/>
+                <br/>
+                With Love,<br/>
+                Sylvie & Bernard
+                `,
+                attachments: [{
+                    filename: 'image.png',
+                    contentType: 'image/png',
+                    content: new Buffer.from(image.split("base64,")[1], "base64"),
+                }]
+            }
+            break;
+        case "french":
+            data = {
+                from: "Tae Kimura <sheepman7893@gmail.com>",
+                to,
+                cc: "Tae Kimura <sheepman7893@gmail.com>",
+                // bcc: "Tae Kimura <sheepman7893@gmail.com>",
+                subject: `Votre Signal de Prospérité Universelle`,
+                html: `
+                Félicitations ! Voici le résultat de votre signal de Prospérité Universelle.
+                <br/>
+                <br/>
+                Avec Amour,<br/>
+                Sylvie & Bernard
                 `,
                 attachments: [{
                     filename: 'image.png',

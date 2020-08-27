c76dee42d58d6e729800c2d2ca8cca9b8fb02fbe
Tae Kimura <sheepman7893@gmail.com>
2020-08-27 16:47:23 -0700

    added-heroku

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ app.use(bodyParser.urlencoded({
 
 app.use(cookieParser());
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 // , parameterLimit: 10000
 
 app.post("/api/sendMail", (req, res) => {

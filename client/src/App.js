816bc9b5c195327b130e6815e366dd369af1fb47
Francois Polo <andasan@gmail.com>
2020-08-28 00:57:43 -0700

    added header for admin and layout fixes on the dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
-import React, {useState,useEffect} from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect'
+import React, { useState, useEffect } from 'react';
+import { Switch, Route, Redirect, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect'
+
+import './App.scss';
 
 import HomePage from './pages/homepage/HomePage';
 import WheelPage from './pages/wheel/WheelPage';
@@ -10,16 +12,16 @@ import Header from './components/Header/Header.component';
 import Dashboard from './components/Dashboard/Dashboard.component';
 import { auth, createUserProfileDocument } from './components/firebase/firebase_utils';
 import { setCurrentUser } from './redux/user/user.action';
-import {selectCurrentUser} from './redux/user/user.selector';
+import { selectCurrentUser } from './redux/user/user.selector';
 import Loading from './components/Loading/Loading';
 
-function App({setCurrentUser,currentUser}) {
+function App({ setCurrentUser, currentUser }) {
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(()=>{
+  useEffect(() => {
     let unsubscribeFromAuth = null;
-    unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
-      if(userAuth){
+    unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapShot => {
           setCurrentUser({
@@ -28,7 +30,7 @@ function App({setCurrentUser,currentUser}) {
           });
         });
         setIsLoading(false);
-      }else{
+      } else {
         setCurrentUser(userAuth);
         setIsLoading(false);
       }
@@ -36,18 +38,26 @@ function App({setCurrentUser,currentUser}) {
     return () => {
       unsubscribeFromAuth();
     };
-  },[setCurrentUser]);
+  }, [setCurrentUser]);
 
-  if(isLoading) return(<Loading />)
+  if (isLoading) return (<Loading />)
 
   return (
     <>
+      {
+        currentUser && currentUser.role === "admin" && (
+          <div className="admin-header">
+            Hi, <b>{currentUser.displayName}</b>. <Link to="/admin" className="admin-link">Go to admin dashboard</Link>
+          </div>
+        )
+      }
+
       <Header />
       <Switch>
         <Route exact path='/' render={() => !currentUser ? (<HomePage />) : (<WheelPage />)} />
         <Route path='/wheel' render={() => !currentUser ? (<Redirect to="/signin" />) : (<WheelPage />)} />
         <Route exact path='/signin' render={() => currentUser ? (<Redirect to="/wheel" />) : (<SignInAndSignUpPage />)} />
-        {currentUser && <Route path='/admin' render={()=> currentUser.role === "admin" ? (<Dashboard />): (<WheelPage />)} />}
+        {currentUser && <Route path='/admin' render={() => currentUser.role === "admin" ? (<Dashboard />) : (<WheelPage />)} />}
       </Switch>
     </>
   );
@@ -62,4 +72,4 @@ const mapDispatchtoProps = dispatch => ({
   // functionName: (x) => {functionName(x)}
 });
 
-export default connect(mapStateToProps,mapDispatchtoProps)(App);
+export default connect(mapStateToProps, mapDispatchtoProps)(App);
--- client/src/components/Dashboard/Dashboard.component.jsx
+++ client/src/components/Dashboard/Dashboard.component.jsx
@@ -6,9 +6,11 @@ import AccordionDetails from "@material-ui/core/AccordionDetails";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import DashboardTable from './DashboardTable.component';
+import DashboardTable from "./DashboardTable.component";
 import Loading from "../Loading/Loading";
 import Pagination from "@material-ui/lab/Pagination";
+import Media from "react-media";
+import moment from 'moment';
 
 import { getAllResults } from "../firebase/firebase_utils";
 
@@ -20,9 +22,6 @@ const Dashboard = () => {
   const itemsPerPage = 10;
   const [page, setPage] = useState(1);
   const [noOfPages, setNoOfPages] = useState();
-  // const [noOfPages] = useState(
-  //   Math.ceil(resultData.length / itemsPerPage)
-  // );
 
   useEffect(() => {
     getAllResults().then((data) => {
@@ -37,60 +36,99 @@ const Dashboard = () => {
     setExpanded(false);
   };
 
-
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
 
-  if(isLoading) return(<Loading />)
+  if (isLoading) return <Loading />;
 
   return (
     <div className={classes.root}>
-      <div className={classes.wrapper}>
-        {resultData &&
-          resultData.slice((page - 1) * itemsPerPage, page * itemsPerPage).map((item, panel) => (
-            <Accordion
-              key={panel}
-              expanded={expanded === `panel${panel}`}
-              onChange={handleChange(`panel${panel}`)}
-            >
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls={`panel${panel}bh-content`}
-                id={`panel${panel}bh-header`}
-              >
-                <Typography className={classes.heading}>
-                  {item.displayName}
+      <Media
+        queries={{
+          desktop: "(min-width: 771px)",
+          largeDesktop: "(min-width: 1024px)"
+        }}
+      >
+        {(matches) => (
+          <div className={classes.wrapper}>
+            <h1>Quiz Result List</h1>
+            <Divider />
+            <br />
+            <AccordionDetails>
+              <Typography component={'span'} className={classes.heading}>Category</Typography>
+              {matches.desktop && (
+                <Typography component={'span'} className={classes.secondaryHeading}>
+                  Email
                 </Typography>
-                <Typography className={classes.secondaryHeading}>
-                  {item.email}
+              )}
+              <Typography component={'span'} className={classes.secondaryHeading}>
+                Score
+              </Typography>
+              {matches.largeDesktop && (
+                <Typography component={'span'} className={classes.secondaryHeading}>
+                  Create At
                 </Typography>
-                <Typography className={classes.secondaryHeading}>
-                  {item.total}
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails className={classes.dropDown}>
-                <Typography >
-                  <DashboardTable data={item.aveAnswers} />
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-          ))}
-          <Divider />
-      <Box component="span">
-        <Pagination
-          count={noOfPages}
-          page={page}
-          onChange={handlePageChange}
-          defaultPage={1}
-          color="secondary"
-          size="large"
-          showFirstButton
-          showLastButton
-          classes={{ ul: classes.paginator }}
-        />
-      </Box>
-      </div>
+              )}
+            </AccordionDetails>
+            {resultData &&
+              resultData
+                .slice((page - 1) * itemsPerPage, page * itemsPerPage)
+                .map((item, panel) => (
+                  <Accordion
+                    key={panel}
+                    expanded={expanded === `panel${panel}`}
+                    onChange={handleChange(`panel${panel}`)}
+                  >
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls={`panel${panel}bh-content`}
+                      id={`panel${panel}bh-header`}
+                    >
+                      <Typography component={'span'} className={classes.heading}>
+                        {item.displayName}
+                      </Typography>
+                      {matches.desktop && (
+                        <Typography component={'span'} className={classes.secondaryHeading}>
+                          {item.email}
+                        </Typography>
+                      )}
+                      <Typography component={'span'} className={classes.secondaryHeading}>
+                        {item.total}
+                      </Typography>
+                      {matches.largeDesktop && (
+                        <Typography component={'span'} className={classes.secondaryHeading}>
+                          {moment(item.createdAt.toDate()).format('lll')}
+                        </Typography>
+                      )}
+                    </AccordionSummary>
+                    <AccordionDetails className={classes.dropDown}>
+                      <Typography component={'span'} className={classes.dropDownWrapper}>
+                        <DashboardTable
+                          data={item.aveAnswers}
+                          total={item.total}
+                        />
+                      </Typography>
+                    </AccordionDetails>
+                  </Accordion>
+                ))}
+            <Divider />
+            <Box component="span">
+              <Pagination
+                count={noOfPages}
+                page={page}
+                onChange={handlePageChange}
+                defaultPage={1}
+                color="secondary"
+                size="large"
+                showFirstButton
+                showLastButton
+                classes={{ ul: classes.paginator }}
+              />
+            </Box>
+          </div>
+        )}
+      </Media>
     </div>
   );
 };
@@ -109,21 +147,33 @@ const useStyles = makeStyles((theme) => ({
   },
   heading: {
     fontSize: theme.typography.pxToRem(15),
-    flexBasis: "33.33%",
+    flexBasis: "25%",
     flexShrink: 0,
+  },
+  headingSpacer: {
+    width: "64px",
   },
   secondaryHeading: {
     fontSize: theme.typography.pxToRem(15),
-    flexBasis: "33.33%",
+    flexBasis: "25%",
     color: theme.palette.text.secondary,
+    flexShrink: 0,
+    textAlign: "center"
   },
   dropDown: {
     display: "flex",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    background: "#111",
+  },
+  dropDownWrapper: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
   },
   paginator: {
     justifyContent: "center",
-    padding: "10px"
-  }
+    padding: "10px",
+  },
+  tableRow: {},
 }));

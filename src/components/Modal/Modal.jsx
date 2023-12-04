25b050eeafa14003c0da281c5b2419f2126c40d6
Tae Kimura <sheepman7893@gmail.com>
2023-12-03 16:38:04 -0800

    fix: update package.json

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import { Context } from "../../pages/wheel/WheelPage";
 import { Modal } from "react-responsive-modal";
 import { Button } from "reactstrap";
-import TranlationButton from "../TranslationButton/TranslationButton";
+import TranslationButton from "../TranslationButton/TranslationButton";
 import "react-responsive-modal/styles.css";
 import "./Modal.scss";
 
@@ -24,7 +24,7 @@ const QuestionModal = () => {
     const startQuestionnaire = () => {
         return (
             <>
-                <TranlationButton />
+                <TranslationButton />
                 <br />
                 <h3 style={{ textAlign: "center", padding: "0.8em 0em 0.3em 0", fontFamily: 'Open Sans', fontWeight: "600", color: "#3d2903" }}>{title}</h3>
                 <p id="instruction" style={{ padding: "0em 0em 1em 0em", color: "#3d2903", fontSize: "1.1em" }}>
